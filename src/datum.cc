00eb3eaa990a61f36651aaf2b8663e6fb9cc461c
Matt Broadstone <mbroadst@gmail.com>
2016-07-24 12:37:20 -0400

    refactor(datum): allow default construction of Datum
    Since we don't have access to something like boost::optional, we
    can assign a Datum a type of INVALID in order to optionally require
    it as a parameter. This is particularly useful for the upcoming
    introduction of the internal Query class.

--- src/datum.cc
+++ src/datum.cc
@@ -250,7 +250,7 @@ int Datum::compare(const Datum& other) const {
     COMPARE_OTHER(type);
     int c;
     switch (type) {
-    case Type::NIL: break;
+    case Type::NIL: case Type::INVALID: break;
     case Type::BOOLEAN: COMPARE_OTHER(value.boolean); break;
     case Type::NUMBER: COMPARE_OTHER(value.number); break;
     case Type::STRING:
@@ -283,6 +283,8 @@ int Datum::compare(const Datum& other) const {
             COMPARE(c, 0);
         }
         break;
+    default:
+        throw Error("cannot compare invalid datum");
     }
     return 0;
 #undef COMPARE_OTHER
@@ -399,6 +401,8 @@ void Datum::write_json(json_writer_t *writer) const {
     case Type::TIME:
         to_raw().write_json(writer);
         break;
+    default:
+        throw Error("cannot write invalid datum");
     }
 }
 
