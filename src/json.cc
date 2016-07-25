4f063bef3fe97f0864104c735e17c136362f25a8
Matt Broadstone <mbroadst@gmail.com>
2016-07-25 11:16:32 -0400

    perf(json): reserve array size before adding elements
    Conflicts:
    	src/json.cc

--- src/json.cc
+++ src/json.cc
@@ -23,7 +23,8 @@ Datum read_datum(const rapidjson::Value &json) {
     case rapidjson::kTrueType: return true;
     case rapidjson::kNumberType: return json.GetDouble();
     case rapidjson::kStringType:
-        return Datum(std::string(json.GetString(), json.GetStringLength()));
+        return std::string(json.GetString(), json.GetStringLength());
+
     case rapidjson::kObjectType: {
         Object result;
         for (rapidjson::Value::ConstMemberIterator it = json.MemberBegin();
